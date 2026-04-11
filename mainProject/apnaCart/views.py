f4ec1ee551a8f214f1790640590b40c1c887efaa
Yashvi Hirani <yashvihirani0408@gmail.com>
2026-04-11 09:08:42 +0530

    v0.1

---
+++ mainProject/apnaCart/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "landingPage.html") 
+
+def aiGroceryPlanner(request):
+    return render(request, "AIGroceryPlanner.html") 
+
+def myOrder(request):
+    return render(request, "myOrder.html")
